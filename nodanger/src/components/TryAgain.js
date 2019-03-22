fe057c600a4c3948fa7b328d45ca167641acba39
peterhl <peterhleung8@gmail.com>
2019-03-21 23:55:49 -0500

    Add component for incorrect username

---
+++ nodanger/src/components/TryAgain.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function TryAgain(props) {
+    return (
+        <div className="tryagain">
+            <h2 style={{"color":"red"}}>
+                Please try a different Username/Password
+            </h2>
+        </div>
+    )
+}
+
+export default TryAgain
